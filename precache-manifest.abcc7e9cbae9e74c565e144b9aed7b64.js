b14bb079516c391f9e718df13053f95b2ff4cb1b
THUHAI1010 <thuhainguyen1010@gmail.com>
2021-10-21 23:04:52 +0700

    Updates

---
+++ precache-manifest.abcc7e9cbae9e74c565e144b9aed7b64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9747c8aa6c860f4851c150276186df6c",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "11a15c2391bb3f9ed98d",
+    "url": "/todoapp/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "06abae81a00c8d9ace8e",
+    "url": "/todoapp/static/css/main.5820d271.chunk.css"
+  },
+  {
+    "revision": "11a15c2391bb3f9ed98d",
+    "url": "/todoapp/static/js/2.ca743ca3.chunk.js"
+  },
+  {
+    "revision": "06abae81a00c8d9ace8e",
+    "url": "/todoapp/static/js/main.b255231a.chunk.js"
+  },
+  {
+    "revision": "3898a5fd3fd40dce9445",
+    "url": "/todoapp/static/js/runtime~main.00cdf33a.js"
+  }
+]);
